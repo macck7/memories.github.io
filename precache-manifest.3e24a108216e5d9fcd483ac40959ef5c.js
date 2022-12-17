97fd7ec57222b5933dc642042448efef5db3637c
Mohit Kumar Gupta <49000628+macck7@users.noreply.github.com>
2022-12-17 12:21:46 +0530

    commit

---
+++ precache-manifest.3e24a108216e5d9fcd483ac40959ef5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6b51a8fd78df0314093eecb297001b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c889a890a6dc3e9d1f",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "94722200f2bb2c54f87b",
+    "url": "/static/js/2.32d91e32.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.32d91e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c889a890a6dc3e9d1f",
+    "url": "/static/js/main.76b7183a.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
